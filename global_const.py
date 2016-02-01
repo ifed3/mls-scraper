c56098781d8ab5186468971a061fbc87e0039aa6
Ife Ajiboye <ajiboye_ifedayo@yahoo.com>
2016-02-01 14:40:20 -0800

    Move user agent details to global constant page

--- global_const.py
+++ global_const.py
@@ -2,6 +2,8 @@ BASE_URL = "http://www.craigslist.org/"
 EAST_BAY_APT = "search/eby/apa"
 GECODOING_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
 OFFSET = 2400
+#Periodically update the operating system and broswer version number
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel MAC OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
 
 class Listing:
     def __init__(self):
