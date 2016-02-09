50c469f8123f5797d5b235dda5628ace6dd11bf7
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-02-08 18:30:00 -0800

    Centralize global variables into the globals file

--- global_const.py
+++ global_const.py
@@ -3,6 +3,11 @@ OFFSET = 2400
 #Periodically update the operating system and broswer version number
 #Create array of different user agents to be randomly selected from
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel MAC OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
+IP_CHECK_ADDR = "https://www.atagar.com/echo.php"
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+SOCKS_PORT = 8080
+SOCKS_HOST = "222.114.148.47"
 
 class Listing:
     def __init__(self):
@@ -21,3 +26,16 @@ class Listing:
         self.post_id=""
         self.repost_of=""
         self.database_input_date=""
+
+#initialize global variables
+def init():
+    global shadow_db
+    shadow_db = ""
+    global city_table
+    city_table = ""
+    global city_name
+    city_name = ""
+    global city_url
+    city_url = ""
+    global csv_directory
+    csv_directory = ""
