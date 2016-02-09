fe5c80dd87637d657cd00d5bf2a8ffd06aaa4461
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-02-08 18:31:37 -0800

    Partition code to enable gui to be main entry point

--- scrape.py
+++ scrape.py
@@ -5,22 +5,14 @@ import csv, json, sys, os
 import pymongo
 from pymongo import MongoClient
 
-def request_user_input():
-    global city_name
-    #city_name = raw_input("Enter location name: ")
-    city_name = "Humboldt"
-    global city_url
-    #city_url = raw_input("Enter url referring to location (eg. for Chicago, https://chicago.craigslist.org/search/chc/apa): ")
-    city_url = "https://humboldt.craigslist.org/search/apa"
-    global csv_directory
-    #csv_directory = raw_input("Enter folder to save results to csv file: ")
-    csv_directory= "/Users/ifed3/Documents/Development/Repositories/shadow-mls-crawler"
-
-def create_database(city_name):
+def create_database():
     client = MongoClient()
-    client = MongoClient('localhost', 27017)
+    client = MongoClient(global_const.MONGO_HOST, global_const.MONGO_PORT)
     db = client.shadow_market
-    collection = db[city_name]
+    return db
+
+def create_datatable(database, city_name):
+    collection = database[city_name]
     collection.create_index([('post_id', pymongo.ASCENDING)], unique=True)
     return collection
 
@@ -33,16 +25,16 @@ def retrieve_urls_from_database(collection):
     return url_list
 
 def scrape_site(city_table, city_url, city_name, url_list):
-    listings = spider.create_page_listings(city_name, city_url, url_list)
+    listings = spider.create_page_listings(global_const.city_name, global_const.city_url, url_list)
     for listing in listings:
-        spider.populate_from_listing_page(listing, city_table)
+        spider.populate_from_listing_page(listing, global_const.city_table)
 
 def write_csv_file(csv_directory, city_name, city_table):
     #Retrieve all documents from db (without the id field)
-    documents = city_table.find(projection={'_id':False})
+    documents = global_const.city_table.find(projection={'_id':False})
     #Convert json from db to csv
     #JSON represents flat object, if that changes in the future, this code will not be entirely functional
-    csv_filename = os.path.join(csv_directory, city_name + " Shadow.csv")
+    csv_filename = os.path.join(global_const.csv_directory, global_const.city_name + " Shadow.csv")
     with open(csv_filename, "wb+") as myfile:
         #Use dict writer in csv creation to ensure desired order
         fieldnames = ["city", "description", "url", "footage", "zipcode", "bed", "bath",
@@ -52,24 +44,29 @@ def write_csv_file(csv_directory, city_name, city_table):
         for document in documents:
             wr.writerow(document)
 
-request_user_input()
-user_agent.create_user_agent()
-print spider.send_request("https://www.atagar.com/echo.php").read() #Get ip address
-global city_table
-city_table = create_database(city_name)
-url_list = retrieve_urls_from_database(city_table)
-scrape_site(city_table, city_url, city_name, url_list)
-write_csv_file(csv_directory, city_name, city_table)
-# except Exception, e:
-#     print str(e)
-# finally:
-#     write_csv_file(csv_directory, city_name, city_table)
+def set_up():
+    global_const.init()
+    user_agent.create_user_agent()
+    print spider.send_request(global_const.IP_CHECK_ADDR).read() #Get ip address
+    global_const.shadow_db = create_database()
+
+def main():
+    try:
+        global_const.city_table = create_datatable(global_const.shadow_db, global_const.city_name)
+        url_list = retrieve_urls_from_database(global_const.city_table)
+        scrape_site(global_const.city_table, global_const.city_url, global_const.city_name, url_list)
+        write_csv_file(global_const.csv_directory, global_const.city_name, global_const.city_table)
+    except Exception, e:
+        print str(e)
+    finally:
+        print "Scraping is complete"
+        write_csv_file(global_const.csv_directory,
+                        global_const.city_name, global_const.city_table)
 
 # #Goes to the main city page
-# #Use for gui aspect
-# # city_name = open(sys.argv[0])
-# # city_url = open(sys.argv[1])
-# # csv_directory = open(sys.argv[2])
+# global_const.city_name = "San Jose"
+# global_const.city_url = "https://humboldt.craigslist.org/search/apa"
+# global_const.csv_directory= "/Users/ifed3/Documents/Development/Repositories/shadow-mls-crawler"
 # https://chicago.craigslist.org/search/chc/apa
 # /Users/ifed3/Documents/Development/Repositories/shadow-mls-crawler
 
